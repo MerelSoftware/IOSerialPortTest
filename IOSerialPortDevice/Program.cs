62795056a72125ea65ea3cee438e11ec24f0ce2b
Steven Merel <steven.merel@gmx.de>
2019-03-19 22:52:16 +0100

    Initial Commit

---
+++ IOSerialPortDevice/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO.Ports;
+
+namespace IOSerialPortDevice
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            return MainAsync(args).GetAwaiter().GetResult();
+        }
+
+        static async Task<int> MainAsync(string[] args)
+        {
+            SerialPort serialPort = new SerialPort("COM2", 9600, Parity.None, 8, StopBits.One);
+            serialPort.NewLine = "\r";
+            serialPort.Open();
+
+            Console.WriteLine("> DEVICE STARTED");
+            bool _continue = true;
+            String input
+                 , output;
+
+            Random rng = new Random();
+
+            do
+            {
+                input = await ReadDataAsync(serialPort);
+                Console.WriteLine($">> {input.Length}:{input.ToUpper()}");
+
+                switch (input.ToUpper())
+                {
+                    case "NUL":
+
+                        output = $"{input.PadRight(3, ' ').Substring(0, 3).Trim().ToUpper()}x";
+                        serialPort.WriteLine(output);
+                        Console.WriteLine($"<< {output.Length}:{output}");
+
+                        output = "DEV";
+                        serialPort.WriteLine(output);
+                        Console.WriteLine($"<< {output.Length}:{output}");
+
+                        System.Threading.Thread.Sleep(5000);
+
+                        output = "STR";
+                        serialPort.WriteLine(output);
+                        Console.WriteLine($"<< {output.Length}:{output}");
+
+                        break;
+
+                    case "BYE":
+                        _continue = false;
+                        break;
+
+                    default:
+                        output = $"{input.PadRight(3, ' ').Substring(0, 3).Trim().ToUpper()}x";
+                        serialPort.WriteLine(output);
+                        Console.WriteLine($"<< {output.Length}:{output}");
+                        break;
+                }
+            } while (_continue);
+            serialPort.Write($"KTHXBYE");
+            Console.WriteLine("> DEVICE STOPPED");
+            return 0;
+        }
+
+        private static async Task<string> ReadDataAsync(SerialPort serialPort)
+        {
+            byte[] buffer = new byte[serialPort.ReadBufferSize];
+
+            int bytesRead = await serialPort.BaseStream.ReadAsync(buffer, 0, buffer.Length);
+
+            return Encoding.ASCII.GetString(buffer).Substring(0, bytesRead - serialPort.NewLine.Length);
+        }
+    }
+}
--- IOSerialPortTest/Program.cs
+++ IOSerialPortTest/Program.cs
@@ -1,15 +1,90 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO.Ports;
 
 namespace IOSerialPortTest
 {
     class Program
     {
-        static void Main(string[] args)
+        static int Main(string[] args)
         {
+            return MainAsync(args).GetAwaiter().GetResult();
+        }
+
+        static async Task<int> MainAsync(string[] args)
+        {
+            SerialPort serialPort = new SerialPort("COM3", 9600, Parity.None, 8, StopBits.One);
+            serialPort.NewLine = "\r";
+            serialPort.Open();
+
+            serialPort.DiscardInBuffer();
+            serialPort.DiscardOutBuffer();
+
+            Console.WriteLine("> Connected");
+
+            string[] answer = default(string[]);
+            serialPort.WriteLine("CON");
+            answer = await ReadDataAsync(serialPort);
+            HandleData(answer);
+            serialPort.WriteLine("AUX");
+            answer = await ReadDataAsync(serialPort);
+            HandleData(answer);
+
+            bool _continue = true;
+            do
+            {
+                //String input = Console.ReadLine();
+                //serialPort.WriteLine(input);
+                //answer = await ReadDataAsync(serialPort);
+                //HandleData(answer);
+
+                serialPort.WriteLine("NUL");
+                answer = await ReadDataAsync(serialPort);
+                HandleData(answer);
+
+                answer = await ReadDataAsync(serialPort);
+                if(answer[0] == "DEV")
+                {
+                    HandleData(answer);
+
+                    answer = await ReadDataAsync(serialPort);
+                    HandleData(answer);
+                }
+
+                System.Threading.Thread.Sleep(1000);
+
+            } while (_continue);
+            Console.Write("> Disconnected");
+            return 0;
+        }
+
+        private static void HandleData(string[] data)
+        {
+            for (int idx = 0, lgt = data.Length; idx < lgt; idx++)
+            {
+                if (data[idx] == string.Empty) continue;
+
+                ConsoleColor bgOld = Console.BackgroundColor
+                           , fgOld = Console.ForegroundColor;
+
+                Console.BackgroundColor = fgOld;
+                Console.ForegroundColor = bgOld;
+                Console.WriteLine($"<< {data[idx]}");
+
+                Console.BackgroundColor = bgOld;
+                Console.ForegroundColor = fgOld;
+            }
+        }
+
+
+        private static async Task<string[]> ReadDataAsync(SerialPort serialPort)
+        {
+            byte[] buffer = new byte[serialPort.ReadBufferSize];
+
+            int bytesRead = await serialPort.BaseStream.ReadAsync(buffer, 0, buffer.Length);
+            serialPort.DiscardInBuffer();
+            return Encoding.ASCII.GetString(buffer).Substring(0, bytesRead).Split(serialPort.NewLine.ToCharArray());
         }
     }
 }
